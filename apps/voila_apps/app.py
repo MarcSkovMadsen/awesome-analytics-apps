bf19ffbdf2a76eb63d95416637b21b30eda381a3
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-27 09:48:02 +0100

    changes

--- apps/voila_apps/app.py
+++ apps/voila_apps/app.py
@@ -5,22 +5,43 @@ import qgrid
 from awesome_analytics_apps import stack_overflow
 from typing import Optional, List
 import qgrid
+import styles
+from plotly import express as px
+from markdown import markdown
 
 IPYTHON_DISPLAY_DOCS = (
     "https://ipython.readthedocs.io/en/stable/api/generated/IPython.display.html"
 )
-IPYTHON_WIDGETS_DOCS = (
+IPYWIDGETS_DOCS = (
     "https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20List.html"
 )
 
+QGRID_DOCS = "https://qgrid.readthedocs.io/en/latest/index.html"
+
 
 def main():
-    configure_pandas()
+    configure()
+
+    header_area = [widgets.HTML(markdown("# Awesome Analytics Apps in Voila"))]
+    sidebar_area = [
+        widgets.HTML(
+            markdown(
+                f"""
+## Resources
+
+- [IPython display Docs]({IPYTHON_DISPLAY_DOCS})
+- [IPy Widgets Docs]({IPYWIDGETS_DOCS})
+- [QGrid Docs]({QGRID_DOCS})
+"""
+            )
+        )
+    ]
+    ip.display(*header_area)
+    ip.display(*sidebar_area)
+
     ip.display(
         ip.Markdown(
-            """# Awesome Analytics Apps in Jupyter Voila
-
-## Introduction
+            """## Introduction
 
 You can use Python and Streamlit as shown in the code below to create a web app!
 """
@@ -37,9 +58,7 @@ ip.display(
 """,
             language="python3",
         ),
-        ip.Markdown(
-            "For more info watch the ***30 minutes introduction*** to Streamlit"
-        ),
+        ip.Markdown("For more info watch the ***30 minutes introduction*** to Voila"),
         ip.YouTubeVideo("VtchVpoSdoQ"),
     )
 
@@ -48,11 +67,26 @@ ip.display(
 
     stack_overflow_component(schema, results)
 
+    # layout_page(header_area, sidebar_area, main_area, footer_area)
 
-def configure_pandas():
+
+def configure():
     pd.set_option("display.max_columns", 1000)  # or 1000
     pd.set_option("display.max_rows", 100)  # or 1000
     pd.set_option("display.max_colwidth", -1)
+
+    # Todo: Change 1000 to styles.MAX_WIDTH
+    ip.display(
+        ip.HTML(
+            """
+    <style type="text/css">
+    .q-grid-container {
+        max-width: 1000 px;
+    }
+    </style>
+    """
+        )
+    )
 
 
 def run_all_code_below(input):
@@ -67,6 +101,7 @@ def run_all_code_below_button():
 
 def stack_overflow_component(schema: pd.DataFrame, results: pd.DataFrame):
     """The Stack Overflow compontent writes the Questions, Results and a Distribution"""
+
     ip.display(
         ip.Markdown(
             f"""
@@ -75,7 +110,7 @@ def stack_overflow_component(schema: pd.DataFrame, results: pd.DataFrame):
 You will be analyzing and providing insights from the Stack Overflow 2019 survey
 
 <a href="{stack_overflow.SURVEY_2019_URL}" target="_blank">
-<img src="{stack_overflow.IMAGE_2019_URL}"style="width:100%;">
+<img src="{stack_overflow.IMAGE_2019_URL}"style="height=300px;">
 </a>
 
 Results: [{stack_overflow.SURVEY_2019_URL}]({stack_overflow.DATA_URL})
@@ -85,19 +120,20 @@ Data: [{stack_overflow.DATA_URL}]({stack_overflow.DATA_URL})
         )
     )
 
-    questions = stack_overflow_questions_component(schema)
-    stack_overflow_answers_component(results, questions)
+    questions_grid = stack_overflow_questions_component(schema)
+    stack_overflow_answers_component(results, questions_grid)
+    respondents_per_country_component(results)
 
 
-def stack_overflow_questions_component(schema: pd.DataFrame) -> widgets.SelectMultiple:
-    """This component writes the Stack Overflow Developer Questions and returns a selected list of
-    questions
+def stack_overflow_questions_component(schema: pd.DataFrame) -> qgrid.QGridWidget:
+    """This component writes the Stack Overflow Developer Questions
 
     Arguments:
         schema {pd.DataFrame} -- A DataFrame of questions
 
     Returns:
-        Optional[List[str]] -- [description]
+        qgrid.QGridWidget -- Returns the QGridWidget Grid used to show the questions.
+        Can be used to filter answers later on.
     """
     ip.display(
         ip.Markdown(
@@ -108,74 +144,85 @@ def stack_overflow_questions_component(schema: pd.DataFrame) -> widgets.SelectMu
 """
         )
     )
-
-    def show_questions(selected_questions, number_of_rows=10):
-        schema_to_show = schema
-        if selected_questions:
-            schema_to_show = schema_to_show[
-                schema_to_show["Column"].isin(selected_questions)
-            ]
-        schema_to_show = schema_to_show.head(number_of_rows)
-        ip.display(qgrid.show_grid(schema_to_show))
-
-    questions = widgets.SelectMultiple(
-        options=sorted(schema["Column"].unique()),
-        value=[],
-        rows=10,
-        description="Select questions",
-        disabled=False,
+    questions_grid = qgrid.show_grid(
+        schema,
+        column_options=styles.RESULTS_GRID_COL_OPTIONS,
+        column_definitions=styles.RESULTS_GRID_COL_DEFS,
     )
-    questions_number_of_rows = widgets.RadioButtons(
-        options=(10, 85), value=10, description="Select # of Questions", disabled=False
-    )
-    widgets.interact(
-        show_questions,
-        selected_questions=questions,
-        number_of_rows=questions_number_of_rows,
-    )
-
-    return questions
+    # We select something so that some Answers are shown
+    questions_grid.change_selection(rows=[0, 1])
+    ip.display(questions_grid)
+    return questions_grid
 
 
-def stack_overflow_answers_component(
-    results, selected_questions: widgets.SelectMultiple
-):
+def stack_overflow_answers_component(results, questions_grid: qgrid.QGridWidget):
     """This component writes the Stack Overflow Developer Survey Questions
 
     Arguments:
         results {[type]} -- A DataFrame of the Results
-        selected_questions {Optional[List[str]]} -- a list of questions to filter
+        questions_grid {qgrid.QGridWidget} -- The table of questions
     """
 
     ip.display(ip.Markdown("# Stack Overflow Results 2019"))
-    out = widgets.Output()
 
-    def h(number_of_rows_to_show: int, selected_questions: List[str]):
-        results_to_show = results
-        if selected_questions:
-            results_to_show = results_to_show[selected_questions]
-        results_to_show = results_to_show.head(number_of_rows_to_show)
+    def get_selected_questions(questions_grid):
+        return list(questions_grid.get_selected_df()["Column"])
 
-        with out:
-            ip.clear_output()
-            ip.display(qgrid.show_grid(results_to_show))
-
-    number_of_rows_to_show_widget = widgets.Dropdown(
-        description="Select # Answers to show",
-        options=[10, 50, 500, 5000, len(results)],
-        value=10,
+    results_grid = qgrid.show_grid(
+        results[get_selected_questions(questions_grid)].head(10),
+        column_options=styles.RESULTS_GRID_COL_OPTIONS,
+        column_definitions=styles.RESULTS_GRID_COL_DEFS,
+    )
+    no_results_grid = ip.Markdown(
+        "**Select one or more questions in the table above to show the results!**"
     )
 
-    def f(number_of_rows_to_show):
-        h(number_of_rows_to_show, list(selected_questions.value))
+    def update_results_grid():
+        selected_questions = list(questions_grid.get_selected_df()["Column"])
+        if selected_questions:
+            results_to_show = results
+            results_to_show = results_to_show[selected_questions]
+            results_grid.df = results_to_show
+        else:
+            results_grid.df = pd.DataFrame()
 
-    widgets.interact(f, number_of_rows_to_show=number_of_rows_to_show_widget)
+    def questions_grid_handler(change):
+        update_results_grid()
 
-    def g(change):
-        h(number_of_rows_to_show_widget.value, list(change["new"]))
+    questions_grid.observe(handler=questions_grid_handler, names="_selected_rows")
 
-    selected_questions.observe(handler=g, names="value")
-    ip.display(out)
+    ip.display(no_results_grid)
+    ip.display(results_grid)
+
+
+def respondents_per_country_component(results):
+    """This component writes a bar chart showing number of Respondants per Country
+
+    Arguments:
+        results {[type]} -- A DataFrame of the Results
+    """
+    ip.display(
+        ip.Markdown(
+            """### Respondents per Countrys
+
+You can plot using matplot, seaborn, vega lite, plotly and other. Here we have chosen plotly
+"""
+        )
+    )
+    distributions = (
+        (results[["Country", "Respondent"]].groupby("Country").count().reset_index())
+        .sort_values("Respondent")
+        .tail(50)
+    )
+    fig = px.bar(
+        distributions,
+        x="Respondent",
+        y="Country",
+        title="Count",
+        orientation="h",
+        width=styles.MAX_WIDTH,
+    )
+    ip.display(fig)
 
 
 if __name__ == "__main__":
---
+++ apps/voila_apps/styles.py
@@ -0,0 +1,47 @@
+import IPython.display as ip
+import ipywidgets as widgets
+
+MAX_WIDTH = 2000
+RESULTS_GRID_COL_OPTIONS = {"width": 200, "maxWidth": 500}
+RESULTS_GRID_COL_DEFS = {
+    "index": {"width": 50},
+    "QuestionText": {"width": 300},
+    "Respondent": {"width": 120},
+    "MainBranch": {"width": 200},
+}
+
+
+def layout_page(header, sidebar, main_, footer):
+    header_area = widgets.HBox(
+        children=header,
+        layout=widgets.Layout(
+            width="auto", grid_area="header", justify_content="center"
+        ),
+    )
+    sidebar_area = widgets.HBox(
+        children=sidebar, layout=widgets.Layout(width="auto", grid_area="sidebar")
+    )
+    main_area = widgets.HBox(
+        children=main_, layout=widgets.Layout(width="auto", grid_area="main")
+    )
+    footer_area = widgets.HBox(
+        children=footer,
+        layout=widgets.Layout(
+            width="auto", grid_area="footer", justify_content="center"
+        ),
+    )
+
+    page = widgets.GridBox(
+        children=[header_area, main_area, sidebar_area, footer_area],
+        layout=widgets.Layout(
+            width="95%",
+            grid_template_rows="auto auto auto",
+            grid_template_columns="25% 25% 25% 25%",
+            grid_template_areas="""
+                "header header header header"
+                "sidebar main main main "
+                "footer footer footer footer"
+                """,
+        ),
+    )
+    ip.display(page)
